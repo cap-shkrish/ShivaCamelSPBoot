20547d1df14cb16bad5f40b6e8a72bb0f28db108
cap-shkrish <39566861+cap-shkrish@users.noreply.github.com>
2019-05-10 13:38:19 -0500

    test

---
+++ src/main/java/SpringBootDemo/ShivawebserviceprjApplication.java
@@ -0,0 +1,13 @@
+package SpringBootDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShivawebserviceprjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShivawebserviceprjApplication.class, args);
+	}
+
+}
